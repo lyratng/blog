b3906ea42cc81c60284ee1e8f7080af7ea8dcd99
lyratng <lyratng@gmail.com>
2024-12-14 09:09:20 +0800

    test: deploy01

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+  base: '/blog/',
+  publicPath:'/blog/',
   themeConfig: {
     name: 'blog',
   },
