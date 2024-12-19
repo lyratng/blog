57ba3bf3570ab1b93781e058844249d7fd886bc9
lyratng <lyratng@gmail.com>
2024-12-19 12:22:12 +0800

    Updates

---
+++ meta__docs.56788149.async.js
@@ -0,0 +1,7 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[904],{60810:function(a,d,n){n.r(d),n.d(d,{demos:function(){return s}});var o=n(67294),t=n(25223),s={}},11171:function(a,d,n){n.r(d),n.d(d,{demos:function(){return s}});var o=n(67294),t=n(68537),s={}},87799:function(a,d,n){n.r(d),n.d(d,{texts:function(){return t}});var o=n(25223);const t=[{value:"This is a guide example.",paraId:0},{value:"\u58F0\u97F3",paraId:1,tocIndex:1},{value:"\uFF1A\u662F",paraId:1,tocIndex:1},{value:"\u7EB5\u6CE2",paraId:1,tocIndex:1},{value:`
+\u56DB\u4E2A\u7269\u7406\u5C5E\u6027\uFF1A\u97F3\u9AD8 \u529B\u5EA6 \u65F6\u503C \u97F3\u8272
+\u97F3\u4E50\u4F1A\u97F3\u9AD8\uFF1A440Hz \u662F\u4E2D\u97F3\u533A\u7684A\u97F3\u9AD8
+\u6307\u6570\u51FD\u6570 \u5BF9\u6570\u51FD\u6570\uFF1A\u771F\u6570 \u5E95\u6570 \u6362\u5E95\u516C\u5F0F
+$$
+L_p=20log_{10} \\frac{p}{p_0}
+$$`,paraId:1,tocIndex:1}]},51446:function(a,d,n){n.r(d),n.d(d,{texts:function(){return t}});var o=n(68537);const t=[{value:"blog",paraId:0}]}}]);
